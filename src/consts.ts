173eb1ce0d00bbde2d5619aed5f821795bdd246f
Michał Gacek <gacek.michael@outlook.com>
2026-05-24 21:54:55 +0200

    feat: rebranding layout

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Tiptopdesign";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "Full-stack AI consulting and practical engineering by Michał Gacek for business problems that need real execution.";
